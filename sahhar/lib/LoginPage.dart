d2e7dea64cd7d670d5f61910af6f0a2c7c6758e5
JENNIFER <jennifer.shammas.2000@gmail.com>
2023-01-12 08:27:51 +0200

    Login updated

--- sahhar/lib/LoginPage.dart
+++ sahhar/lib/LoginPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sahhar/globals.dart';
 import './SignUp.dart';
 import './AdminDashboard.dart';
 
@@ -8,163 +9,238 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController? txtuser;
+  TextEditingController? txtpass;
+  bool didclick = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    txtuser = new TextEditingController();
+    txtpass = new TextEditingController();
+  }
+
+  bool checkdata() {
+    if (txtuser!.text.isEmpty || txtpass!.text.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-              height: 60,
-              decoration: BoxDecoration(
-                  color: Color(0xFF7E0000),
-                  border: Border.all(
-                    color: Color.fromARGB(255, 255, 255, 255),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 60,
+                decoration: BoxDecoration(
+                    color: Globals.cls,
+                    border: Border.all(
+                      color: Color.fromARGB(255, 255, 255, 255),
+                    ),
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20))),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 18, left: 10),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      '← back',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20),
+                    ),
                   ),
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20))),
-              child: Padding(
-                padding: const EdgeInsets.only(top: 18, left: 10),
-                child: Text(
-                  '← back',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                height: 200,
+                color: Color(0xFF7E0000),
+                child: Center(
+                  child: Text(
+                    "Welcome!",
+                    style: TextStyle(
                       color: Colors.white,
-                      fontSize: 20),
-                ),
-              ),
-            ),
-            Container(
-              height: 200,
-              color: Color(0xFF7E0000),
-              child: Center(
-                child: Text(
-                  "Welcome!",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              padding: EdgeInsets.all(15),
-              child: Column(
-                children: <Widget>[
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      icon: Icon(Icons.person),
-                      labelText: "Username Or Email",
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50),
-                      ),
-                      icon: Icon(Icons.lock),
-                      labelText: "Password",
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 35),
-                  Container(
-                    child: InkWell(
-                      onTap: () {},
-                      child: Container(
-                        width: 170,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Colors.black,
-                          borderRadius: BorderRadius.circular(25),
+              Container(
+                color: Color(0xFF7E0000),
+                child: Container(
+                    width: double.infinity,
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(
+                          color: Colors.white,
                         ),
-                        child: Center(
-                          child: Text(
-                            'LOGIN',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(35),
+                            topRight: Radius.circular(35)))),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(15),
+                child: Column(
+                  children: <Widget>[
+                    TextField(
+                      onChanged: (x) {
+                        txtuser!.text = x;
+                        setState(() {});
+                      },
+                      controller: txtuser,
+                      maxLength: 20,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.person,
+                          size: 25,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        labelText: "Username Or Email",
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      onChanged: (x) {
+                        txtpass!.text = x;
+                        setState(() {});
+                      },
+                      controller: txtpass,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          size: 25,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        // icon: Icon(Icons.lock),
+                        labelText: "Password",
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 35),
+                    didclick == false
+                        ? SizedBox(
+                            height: 10,
+                          )
+                        : Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Invalid Username or password',
+                              style: TextStyle(color: Colors.red),
+                            ),
+                          ),
+                    Container(
+                      child: InkWell(
+                        onTap: checkdata() == false
+                            ? null
+                            : () {
+                                Navigator.pop(context);
+                              },
+                        child: Container(
+                          width: 170,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                          child: Center(
+                            child: Text(
+                              'LOGIN',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  InkWell(
-                    onTap: () {
-                      // Navigate to "Forgot Password" page
-                    },
-                    child: Text(
-                      "Forgot Password?",
-                      style: TextStyle(color: Colors.black),
-                    ),
-                  ),
-                  SizedBox(height: 110),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        child: Image.asset(
-                          'assets/facebook.png',
-                          width: 40,
-                          height: 40,
-                        ),
+                    SizedBox(height: 10),
+                    InkWell(
+                      onTap: () {
+                        // Navigate to "Forgot Password" page
+                      },
+                      child: Text(
+                        "Forgot Password?",
+                        style: TextStyle(color: Colors.black),
                       ),
-                      SizedBox(width: 15),
-                      Container(
-                        child: Image.asset(
-                          'assets/google.png',
-                          width: 40,
-                          height: 40,
+                    ),
+                    SizedBox(height: 110),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          child: Image.asset(
+                            'assets/facebook.png',
+                            width: 40,
+                            height: 40,
+                          ),
                         ),
+                        SizedBox(width: 15),
+                        Container(
+                          child: Image.asset(
+                            'assets/google.png',
+                            width: 40,
+                            height: 40,
+                          ),
+                        ),
+                        SizedBox(width: 15),
+                        Container(
+                          child: Image.asset(
+                            'assets/twitter.png',
+                            width: 40,
+                            height: 40,
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 30),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUp()),
+                        );
+                      },
+                      child: Text(
+                        "Don’t have account? Sign up now.",
+                        style: TextStyle(color: Colors.black),
                       ),
-                      SizedBox(width: 15),
-                      Container(
-                        child: Image.asset(
-                          'assets/twitter.png',
-                          width: 40,
-                          height: 40,
-                        ),
-                      )
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SignUp()),
-                      );
-                    },
-                    child: Text(
-                      "Don’t have account? Sign up now.",
-                      style: TextStyle(color: Colors.black),
                     ),
-                  ),
-                  SizedBox(height: 30),
-                  InkWell(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AdminDashboard()),
-                      );
-                    },
-                    child: Text(
-                      "admin",
-                      style: TextStyle(color: Colors.black),
+                    SizedBox(height: 30),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AdminDashboard()),
+                        );
+                      },
+                      child: Text(
+                        "admin",
+                        style: TextStyle(color: Colors.black),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
