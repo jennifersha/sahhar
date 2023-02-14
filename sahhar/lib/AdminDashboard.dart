d8599805956cf8cf345ad47273e3e1e87aabd326
JENNIFER <jennifer.shammas.2000@gmail.com>
2023-02-14 08:30:38 +0200

    Changed to Sidebar

--- sahhar/lib/AdminDashboard.dart
+++ sahhar/lib/AdminDashboard.dart
@@ -11,140 +11,132 @@ class AdminDashboard extends StatelessWidget {
           'Admin Page',
           style: TextStyle(
             color: Colors.white,
-            fontSize: 30,
             fontWeight: FontWeight.bold,
+            fontSize: 31,
           ),
         ),
         backgroundColor: Color(0xFF7E0000),
         toolbarHeight: 100,
       ),
-      body: Container(
+      body: Center(
+        child: Text('Welcome to the Admin Page'),
+      ),
+      drawer: Drawer(
         child: Column(
           children: [
-            SizedBox(height: 20),
             Container(
-              width: 300,
-              height: 500,
-              child: Drawer(
-                child: Column(
+              height: 150,
+              child: DrawerHeader(
+                child: Row(
                   children: [
-                    Container(
-                      height: 100,
-                      child: DrawerHeader(
-                        child: Row(
-                          children: [
-                            Icon(Icons.supervised_user_circle),
-                            SizedBox(width: 10),
-                            Text(
-                              'Admin Dashboard',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color(0xFF7E0000),
-                              ),
-                            ),
-                          ],
-                        ),
+                    Icon(Icons.supervised_user_circle),
+                    SizedBox(width: 10),
+                    Text(
+                      'Admin Dashboard',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFF7E0000),
                       ),
                     ),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        height: 50,
-                        child: Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text(
-                              'Categories',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color(0xFF7E0000),
-                              ),
-                            ),
-                          ],
-                        ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Text(
+                      'Categories',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Color(0xFF7E0000),
                       ),
                     ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => AddCategory()),
-                        );
-                      },
-                      child: Container(
-                        height: 50,
-                        child: Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text(
-                              'Add Category',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color(0xFF7E0000),
-                              ),
-                            ),
-                          ],
-                        ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddCategory()),
+                );
+              },
+              child: Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Text(
+                      'Add Category',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Color(0xFF7E0000),
                       ),
                     ),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        height: 50,
-                        child: Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text(
-                              'Products',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color(0xFF7E0000),
-                              ),
-                            ),
-                          ],
-                        ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Text(
+                      'Products',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Color(0xFF7E0000),
                       ),
                     ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => AddProduct()),
-                        );
-                      },
-                      child: Container(
-                        height: 50,
-                        child: Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text(
-                              'Add Product',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color(0xFF7E0000),
-                              ),
-                            ),
-                          ],
-                        ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddProduct()),
+                );
+              },
+              child: Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Text(
+                      'Add Product',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Color(0xFF7E0000),
                       ),
                     ),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        height: 50,
-                        child: Row(
-                          children: [
-                            SizedBox(width: 20),
-                            Text(
-                              'Orders Schedule',
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Color(0xFF7E0000),
-                              ),
-                            ),
-                          ],
-                        ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    Text(
+                      'Orders Schedule',
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Color(0xFF7E0000),
                       ),
                     ),
                   ],
