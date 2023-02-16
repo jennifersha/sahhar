1c2d79b4ba231d4bb1a2fee301528e61d0b0e54d
JENNIFER <jennifer.shammas.2000@gmail.com>
2023-02-16 10:45:35 +0200

    AppUser added

--- sahhar/lib/globals.dart
+++ sahhar/lib/globals.dart
@@ -2,4 +2,11 @@ import 'package:flutter/animation.dart';
 
 class Globals {
   static Color cls = Color(0xFF7E0000);
+  static AppUser appuser = new AppUser();
+}
+
+class AppUser {
+  String? name;
+  String? password;
+  String? email;
 }
