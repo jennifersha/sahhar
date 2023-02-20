bc6555ef7cd5e53fc4da1dcf207a897afd5898cb
JENNIFER <jennifer.shammas.2000@gmail.com>
2023-02-20 16:10:46 +0200

    home updated

--- sahhar/lib/Home.dart
+++ sahhar/lib/Home.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
 void main() => runApp(const Home());
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
-  //static const String _title = 'Flutter Code Sample';
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      //title: _title,
       home: MyStatefulWidget(),
     );
   }
@@ -27,10 +27,68 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Home',
-      style: optionStyle,
+  static List<Widget> _widgetOptions = <Widget>[
+    Column(
+      children: [
+        Container(
+          color: Color.fromARGB(255, 206, 205, 205),
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.search),
+                color: Colors.black,
+                onPressed: () {},
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search..',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: StreamBuilder<QuerySnapshot>(
+            stream:
+                FirebaseFirestore.instance.collection("categories").snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: 1, crossAxisCount: 2),
+                itemCount: snapshot.data!.docs.length,
+                padding: EdgeInsets.all(16),
+                itemBuilder: (BuildContext context, int index) {
+                  DocumentSnapshot doc = snapshot.data!.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Container(
+                      color: Color.fromARGB(255, 206, 205, 203),
+                      child: Center(
+                        child: Text(
+                          doc['name'],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ],
     ),
     Text(
       'Likes',
@@ -64,13 +122,18 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                   : _selectedIndex == 2
                       ? 'Cart'
                       : 'Account',
-          style: TextStyle(color: Color.fromARGB(255, 6, 3, 3)),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
         ),
-        backgroundColor: Colors.red,
+        backgroundColor: Color(0xFF7E0000),
+        toolbarHeight: 100,
       ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
+      body: _selectedIndex == 0
+          ? _widgetOptions.elementAt(_selectedIndex)
+          : Center(child: _widgetOptions.elementAt(_selectedIndex)),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
@@ -91,7 +154,8 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
           ),
         ],
         currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
+        selectedItemColor: Color(0xFF7E0000),
+        unselectedItemColor: Colors.black,
         onTap: _onItemTapped,
       ),
     );
