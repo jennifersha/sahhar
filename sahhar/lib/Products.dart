91666baa335760f99830642ad134e77f9b11bc24
JENNIFER <jennifer.shammas.2000@gmail.com>
2023-02-22 10:40:37 +0200

    display products

---
+++ sahhar/lib/Products.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+void main() => runApp(const Products());
+
+class Products extends StatelessWidget {
+  const Products();
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget();
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  // ignore: prefer_final_fields
+  static List<Widget> _widgetOptions = <Widget>[
+    Column(
+      children: [
+        Container(
+          color: Color.fromARGB(255, 206, 205, 205),
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.search),
+                color: Colors.black,
+                onPressed: () {},
+              ),
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Search..',
+                    hintStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: StreamBuilder<QuerySnapshot>(
+            stream:
+                FirebaseFirestore.instance.collection("products").snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(child: CircularProgressIndicator());
+              }
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    childAspectRatio: 1, crossAxisCount: 2),
+                itemCount: snapshot.data!.docs.length,
+                padding: EdgeInsets.all(16),
+                itemBuilder: (BuildContext context, int index) {
+                  DocumentSnapshot doc = snapshot.data!.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Container(
+                      color: Color.fromARGB(255, 206, 205, 203),
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: IconButton(
+                              icon: Icon(Icons.edit),
+                              color: Colors.black,
+                              onPressed: () {},
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: IconButton(
+                              icon: Icon(Icons.delete),
+                              color: Colors.black,
+                              onPressed: () {},
+                            ),
+                          ),
+                          Center(
+                            child: Text(
+                              doc['name'],
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ],
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Products',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Color(0xFF7E0000),
+        toolbarHeight: 100,
+      ),
+      body: _selectedIndex == 0
+          ? _widgetOptions.elementAt(_selectedIndex)
+          : Center(child: _widgetOptions.elementAt(_selectedIndex)),
+    );
+  }
+}
