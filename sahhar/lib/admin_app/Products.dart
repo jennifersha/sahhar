78e173881819db250b7e18493a89adb349c2b340
JENNIFER <jennifer.shammas.2000@gmail.com>
2023-05-11 22:22:46 +0300

    media query added(fix fit with other devices)

--- sahhar/lib/admin_app/Products.dart
+++ sahhar/lib/admin_app/Products.dart
@@ -1,174 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-
 import 'EditProduct.dart';
 
-void main() => runApp(const Products());
-
-class Products extends StatelessWidget {
+class Products extends StatefulWidget {
   const Products();
-
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyStatefulWidget(),
-    );
-  }
+  State<Products> createState() => _ProductsState();
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget();
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  // ignore: prefer_final_fields
-  static List<Widget> _widgetOptions = <Widget>[
-    Column(
-      children: [
-        SizedBox(height: 15),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 226, 226, 226),
-              borderRadius: BorderRadius.circular(30),
-            ),
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.search),
-                  color: Colors.black,
-                  onPressed: () {},
-                ),
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Search..',
-                      hintStyle: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        Expanded(
-          child: StreamBuilder<QuerySnapshot>(
-            stream:
-                FirebaseFirestore.instance.collection("products").snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(child: CircularProgressIndicator());
-              }
-              return GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    childAspectRatio: 1, crossAxisCount: 2),
-                itemCount: snapshot.data!.docs.length,
-                padding: EdgeInsets.all(16),
-                itemBuilder: (BuildContext context, int index) {
-                  DocumentSnapshot doc = snapshot.data!.docs[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        children: [
-                          SizedBox(height: 10),
-                          Container(
-                            height: 80,
-                            width: 80,
-                            child: Image.network(
-                              doc['imageUrl'],
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 6, horizontal: 6),
-                            decoration: BoxDecoration(
-                                //color: Color(0xFF7E0000),
-                                // borderRadius: BorderRadius.circular(20),
-                                ),
-                            child: Stack(
-                              children: [
-                                Align(
-                                  alignment: Alignment.bottomLeft,
-                                  child: IconButton(
-                                    icon: Icon(Icons.edit),
-                                    color: Colors.black,
-                                    onPressed: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => EditProduct(
-                                                  categoryId: doc.id,
-                                                  productId: doc.id,
-                                                )),
-                                      );
-                                    },
-                                  ),
-                                ),
-                                Align(
-                                  alignment: Alignment.bottomRight,
-                                  child: IconButton(
-                                    icon: Icon(Icons.delete),
-                                    color: Colors.black,
-                                    onPressed: () {
-                                      deleteProduct(doc.id);
-                                    },
-                                  ),
-                                ),
-                                Center(
-                                  child: Text(
-                                    doc['name'],
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.normal,
-                                      fontSize: 20,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              );
-            },
-          ),
-        ),
-      ],
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
+class _ProductsState extends State<Products> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           'Products',
           style: TextStyle(
             color: Colors.white,
@@ -176,12 +21,136 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             fontWeight: FontWeight.bold,
           ),
         ),
-        backgroundColor: Color(0xFF7E0000),
-        toolbarHeight: 100,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        )),
       ),
-      body: _selectedIndex == 0
-          ? _widgetOptions.elementAt(_selectedIndex)
-          : Center(child: _widgetOptions.elementAt(_selectedIndex)),
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection("products")
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                        child: CircularProgressIndicator(
+                      color: Color(0xFF7E0000),
+                    ));
+                  }
+                  return GridView.builder(
+                    shrinkWrap: true,
+                    physics: const BouncingScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent:
+                          MediaQuery.of(context).size.width / 1.8,
+                      mainAxisExtent: MediaQuery.of(context).size.height * 0.26,
+                      childAspectRatio: 1,
+                      crossAxisSpacing: 0,
+                      mainAxisSpacing: 0,
+                    ),
+                    itemCount: snapshot.data!.docs.length,
+                    padding: EdgeInsets.zero,
+                    itemBuilder: (BuildContext context, int index) {
+                      DocumentSnapshot doc = snapshot.data!.docs[index];
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 4, vertical: 2),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.all(2),
+                              height: MediaQuery.of(context).size.height * 0.16,
+                              width: MediaQuery.of(context).size.width,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.network(
+                                  doc['imageUrl'][0],
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.08,
+                              width: MediaQuery.of(context).size.width,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Center(
+                                    child: Text(
+                                      doc['name'].toString().toUpperCase(),
+                                      style: const TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Container(
+                                        margin: const EdgeInsets.symmetric(
+                                            horizontal: 4),
+                                        child: InkWell(
+                                          child: const Icon(Icons.edit),
+                                          onTap: () {
+                                            Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) =>
+                                                      EditProduct(
+                                                        // categoryId:
+                                                        //     doc['categories'],
+                                                        productId: doc.id,
+                                                      )),
+                                            );
+                                          },
+                                        ),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.symmetric(
+                                            horizontal: 4),
+                                        child: InkWell(
+                                          child: const Icon(
+                                            Icons.delete,
+                                            color: Colors.red,
+                                          ),
+                                          onTap: () {
+                                            deleteProduct(doc.id);
+                                          },
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
